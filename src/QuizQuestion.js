2079309abe26cc9929a3bd428b8e3d28d4985aaa
ocast <ocast@allstate.com>
2019-04-08 16:00:23 +0100

    Quiz Questions and Handling Events completed

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
-class QuizQuestion extends  Component {
-    render () {
-        return (
-        <main>
+class QuizQuestion extends Component {
+  handleClick (buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
+  }
+  
+  render() {
+    return (
+      <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((answer_options, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_options} 
+              clickHandler={this.handleClick.bind(this)}/>
+            })}
           </ul>
         </section>
       </main>
-        )
-    }
+    )
+  }
 }
 
 export default QuizQuestion;
